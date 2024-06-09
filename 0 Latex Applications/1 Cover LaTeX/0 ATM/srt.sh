5ffb184096f23a293212affb20eb1f3faec0fc16
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-06-09 14:29:40 -0700

    updated automation sorting script Commit by @arfazhxss on Sun 09 Jun 2024

--- 0 Latex Applications/1 Cover LaTeX/0 ATM/srt.sh
+++ 0 Latex Applications/1 Cover LaTeX/0 ATM/srt.sh
@@ -9,10 +9,19 @@ remove_duplicates() {
     fi
 }
 
+# Function to remove entries that are exactly "TEST"
+remove_single_test_entries() {
+    local file="$1"
+    if [[ -f "$file" ]]; then
+        # Use grep to filter out lines that are exactly "TEST"
+        grep -v '^TEST$' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+        echo "Removed single TEST entries from $file"
+    fi
+}
+
 # Export the function so it can be used by find -exec
 export -f remove_duplicates
+export -f remove_single_test_entries
 
 # Find all .txt files recursively and remove duplicates
-find . -type f -name "*.txt" -exec bash -c 'remove_duplicates "$0"' {} \;
-
-
+find . -type f -name "*.txt" -exec bash -c 'remove_duplicates "$0"; remove_single_test_entries "$0"' {} \;
